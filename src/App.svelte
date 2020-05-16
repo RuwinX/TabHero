5fd18a4084cf81c11d6b194272d15b8727ea727a
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-05-16 22:49:49 +0530

    add comment

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,22 @@
 <script>
-import Login from './Login.svelte';
+import TagPages from './components/TagPages.svelte';
+import Main from './components/Main.svelte';
 import User from './_User.js'
+
+const tags = [
+    { text: 'Youtube', added: true },
+    { text: 'Coffee!', added: true },
+    { text: 'Study Philosophy', added: true },
+    { text: 'Events', added: false },
+    { text: 'Reading List', added: false },
+    { text: 'Medium', added: false },
+    { text: 'Design', added: false },
+    { text: 'On The Road', added: false },
+    { text: 'Recipe', added: true },
+    { text: 'Want To Watch', added: false },
+    { text: 'Docs', added: true },
+];
 </script>
 
-<Login></Login>
+<TagPages tags={[...tags, ...tags, ...tags]} />
+<Main />
