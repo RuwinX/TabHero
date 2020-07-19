58ac48a992bc21c85a1046750d676bbe88da0f77
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-07-20 00:10:58 +0530

    add heading component

---
+++ src/components/Heading.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let text = '';
+</script>
+
+<style>
+    h2 {
+        font-family: Avenir;
+        font-weight: 700;
+        font-size: 1.3rem;
+        color: #249c68;
+    }
+</style>
+
+<h2>{text}</h2>
