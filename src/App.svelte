7238abb6a5a3528d89f5dc0c85fcd7bd6cff3c0c
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-08-01 13:28:34 +0530

    init app and sync app state from storage 🥳

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,37 @@
 <script>
-import User from './_User.js'
+    import { onMount } from 'svelte';
+
+    import TopBar from './components/TopBar.svelte';
+    import Main from './pages/Main.svelte';
+
+    import { getCurrentTab, registerOnTabUpdate } from './services/chrome';
+    import { syncTagsState } from './sync';
+
+    let currentTab = {};
+    let currentTabTags = [];
+
+    onMount(async () => {
+        currentTab = await getCurrentTab();
+        currentTabTags = await syncTagsState(currentTab.url);
+
+        const removeListener = registerOnTabUpdate(async (newTab) => {
+            currentTab = await getCurrentTab();
+            currentTabTags = await syncTagsState(currentTab.url);
+        });
+
+        return () => {
+            removeListener();
+        };
+    });
 </script>
+
+<style>
+    .container {
+        width: 300px;
+    }
+</style>
+
+<div class="container">
+    <TopBar state="" user="" />
+    <Main currentTabLink={currentTab.url} tags={currentTabTags} />
+</div>
