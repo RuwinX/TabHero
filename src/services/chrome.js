13513f19792b3b9d2d8b0d9129ee6bc3dc0cd98d
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2020-08-01 13:22:52 +0530

    create tabupdate listener

--- src/services/chrome.js
+++ src/services/chrome.js
@@ -1,3 +1,5 @@
+import { debounce } from 'lodash-es';
+
 export const getCurrentTab = () => {
     return new Promise((resolve, reject) => {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -26,4 +28,36 @@ export const getChromeStorage = (key, defaultVal = []) => {
             resolve(value);
         });
     });
+};
+
+export const registerOnTabUpdate = (handler) => {
+    const listener = debounce((tabId, changeInfo, tab) => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const currentTab = tabs[0];
+            if (tabId === currentTab.id) {
+                // WARNING: tab.* and changeInfo.* properties have very inconsistent values even when triggered repeatedly through the same events.
+                // tab.* seem to be more consistent. changeinfo title, url, favicon always seem to be undefined.
+                const newTab = {
+                    title: tab.title,
+                    url: tab.url,
+                    faviconUrl: tab.favIconUrl
+                };
+                handler(newTab);
+            }
+        })
+    }, 1500);
+
+    /**
+     * WARNING: onUpdated seems to emit 4 to 5 events when a tab changes, each in which changeInfo.status is supposed to be either "unloaded", "loading", or "complete"
+     * BUT in reality changeInfo.status is undefined in most of those events.
+     *
+     * Also observed that the last of those events has the correct tab.title, tab.url, tab.favicon. The preceding events have the outdated values of those fields.
+     * However, this last event always has a changeInfo.status === undefined. So can't filter out this event reliably.
+     * Hence, workaround: debounce that handler to capture just that last event. Debounce with a wait that's more than the typical time diff between two events. Hence wait~1500ms
+     */
+    chrome.tabs.onUpdated.addListener(listener);
+
+    return () => {
+        chrome.tabs.onUpdated.removeListener(listener);
+    };
 };
